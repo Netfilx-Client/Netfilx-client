5a5b44598ed3c82af26100d2c0d1d5d54981b743
guabaaa <gdandtop950923@gmail.com>
2023-04-26 10:26:21 +0900

    초기설정

---
+++ src/types/content.ts
@@ -0,0 +1,14 @@
+/** 콘텐츠 */
+interface contentType {
+  content_id: string;
+  genre: string;
+  title: string;
+  overview: string;
+  season_number: string;
+  episode_number: string;
+  run_time: string;
+  released_at: string;
+  poster_path: any;
+  backdrop_pah: any;
+}
+export type { contentType };
