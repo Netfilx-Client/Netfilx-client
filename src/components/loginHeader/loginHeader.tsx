b065f7b8651d190e79eea74a2299cc1598ef909e
grkimclamos <grkim@clamos.is>
2023-05-03 11:03:04 +0900

    헤더푸터세팅

--- src/components/loginHeader/loginHeader.tsx
+++ src/components/loginHeader/loginHeader.tsx
@@ -1,22 +1,62 @@
 import React, { useEffect, useRef, useState } from "react";
 import Icon from "../../assets/icons/icon";
+import $ from "jquery";
 
 interface LoginHeaderProps {
   children?: React.ReactElement | React.ReactElement[];
 }
 const LoginHeader = (props: LoginHeaderProps) => {
+  const mobileMenu = () => {
+    $(".mobile_ul").slideToggle();
+    console.log(true);
+  };
   return (
     <div className="LoginHeader_container">
       <div className="LoginHeader_left">
         <div className="LoginHeader_logo"></div>
         <div className="LoginHeader_menu">
           <ul>
-            <li>홈</li>
-            <li>시리즈</li>
-            <li>영화</li>
-            <li>NEW! 요즘 대세 콘텐츠</li>
-            <li>내가 찜한 콘텐츠</li>
-            <li>언어별로 찾아보기</li>
+            <li>
+              <a href="">시리즈</a>
+            </li>
+            <li>
+              <a href="">영화</a>
+            </li>
+            <li>
+              <a href="">NEW! 요즘 대세 콘텐츠</a>
+            </li>
+            <li>
+              <a href="">내가 찜한 콘텐츠</a>
+            </li>
+            <li>
+              <a href="">언어별로 찾아보기</a>
+            </li>
+          </ul>
+        </div>
+        <div className="LoginHeader_mobileMenu">
+          <div className="LoginHeader_cbtn" onClick={mobileMenu}>
+            메뉴
+            <Icon.under />
+          </div>
+          <ul className="mobile_ul">
+            <li>
+              <a href="">홈</a>
+            </li>
+            <li>
+              <a href="">시리즈</a>
+            </li>
+            <li>
+              <a href="">영화</a>
+            </li>
+            <li>
+              <a href="">NEW! 요즘 대세 콘텐츠</a>
+            </li>
+            <li>
+              <a href="">내가 찜한 콘텐츠</a>
+            </li>
+            <li>
+              <a href="">언어별로 찾아보기</a>
+            </li>
           </ul>
         </div>
       </div>
@@ -27,7 +67,10 @@ const LoginHeader = (props: LoginHeaderProps) => {
         <div className="LoginHeader_alert">
           <Icon.alert />
         </div>
-        <div className="LoginHeader_profile"></div>
+        <div className="LoginHeader_profile">
+          <img src="" alt="" />
+          <Icon.under />
+        </div>
       </div>
     </div>
   );
